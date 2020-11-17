ff6fca1bdfe203229d1a42c1c64fabfc6cc0262e
Marco Evangelista <marcobjj@users.noreply.github.com>
2020-11-16 21:24:00 -0800

    adds comment model and routes, connect to frontend

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -1,9 +1,15 @@
-const { Pizza } = require('../models');
+const { Pizza, Comment } = require('../models');
 
 const pizzaController = {
     // get all pizzas
     getAllPizza(req, res) {
       Pizza.find({})
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
         .then(dbPizzaData => res.json(dbPizzaData))
         .catch(err => {
           console.log(err);
@@ -14,6 +20,11 @@ const pizzaController = {
     // get one pizza by id
     getPizzaById({ params }, res) {
       Pizza.findOne({ _id: params.id })
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+      .select('-__v')
         .then(dbPizzaData => {
           // If no pizza is found, send 404
           if (!dbPizzaData) {
--- models/Pizza.js
+++ models/Pizza.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat')
 
 const PizzaSchema = new Schema({
     pizzaName: {
@@ -9,14 +10,36 @@ const PizzaSchema = new Schema({
     },
     createdAt: {
       type: Date,
-      default: Date.now
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal)
     },
     size: {
       type: String,
       default: 'Large'
     },
-    toppings: []
+    toppings: [],
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+          }
+      ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+  
+  );
+
+  // get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
   });
+
 
   // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
